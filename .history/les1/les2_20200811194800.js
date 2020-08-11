c07cda21262379f873b80d25e74546200f96d952
Roman Stetsiuk <romansteciuk021@gmail.com>
2020-08-11 20:01:16 +0200

    create websitesLayout branch. Starting makes notes about frontend knowladge. I will try make notes creating professional website.

---
+++ .history/les1/les2_20200811194800.js
@@ -0,0 +1,32 @@
+console.log('Welcome to lesson nr 2');
+
+// Number
+const numberInt = 21;
+const numberFloat = 47.3;
+const numberPow = 10e3;
+
+console.log(`Type Integer: ${numberInt}`);
+console.log(`Type Float: ${numberFloat}`);
+console.log(`Pow (10e3): ${numberPow}`);
+
+console.log(`Exist max value of Integer number. It is ${Number.MAX_SAFE_INTEGER}`);
+console.log(`Exist min value of Integer number. It is ${Number.MIN_SAFE_INTEGER}`);
+
+console.log(`Max integer value in Math is: Math.pow(2,53) - 1 = ${Math.pow(2,53) - 1}`)
+
+console.log(`Max posible value in JS = ${Number.MAX_VALUE}`);
+console.log(`Min posible value in JS = ${Number.MIN_VALUE}`);
+
+console.log(`In JS exist positive infinity(Number.POSITIVE_INFINITY): ${Number.POSITIVE_INFINITY}\n
+exemple infinity is number/0, like 2/0 = ${2/0}`);
+console.log(`In JS exist negative infinity(Number.NEGATIVE_INFINITY): ${Number.NEGATIVE_INFINITY}`);
+
+// Nan
+console.log(`NAN value (Not A Number). Number.NaN = ${Number.NaN}`);
+console.log(typeof NaN);
+const createNanValue = 2 / undefined;
+console.log(Number.isNaN(createNanValue));
+console.log(Number.isNaN(43));
+
+
+
